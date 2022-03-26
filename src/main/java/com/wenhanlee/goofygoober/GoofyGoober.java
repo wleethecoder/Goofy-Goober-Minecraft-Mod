34ff4e658c0c2c36ab2613fe3ba5544d15a5b4ad
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2022-03-25 22:26:41 -0500

    parchmentmc

--- src/main/java/com/wenhanlee/goofygoober/GoofyGoober.java
+++ src/main/java/com/wenhanlee/goofygoober/GoofyGoober.java
@@ -17,18 +17,16 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
-// test
+
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(GoofyGoober.MOD_ID)
-public class GoofyGoober
-{
+public class GoofyGoober {
     public static final String MOD_ID = "goofygoober";
 
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public GoofyGoober()
-    {
+    public GoofyGoober() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the setup method for modloading
         eventBus.addListener(this::setup);
@@ -37,8 +35,7 @@ public class GoofyGoober
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
