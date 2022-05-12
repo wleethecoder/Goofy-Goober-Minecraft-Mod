fb2fd531745bd8e33c9cb3aba343e5f51a4beacb
Wenhan-Samuel-Lee <wenhan_lee@utexas.edu>
2022-05-12 11:45:29 -0500

    may not need packets for the fat feature

--- src/main/java/com/wenhanlee/goofygoober/capabilities/ModCapabilities.java
+++ src/main/java/com/wenhanlee/goofygoober/capabilities/ModCapabilities.java
@@ -1,6 +1,5 @@
 package com.wenhanlee.goofygoober.capabilities;
 
-import com.wenhanlee.goofygoober.capabilities.fat.IFat;
 import com.wenhanlee.goofygoober.capabilities.time.ITimeCounter;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -9,7 +8,5 @@ import net.minecraftforge.common.capabilities.CapabilityToken;
 public class ModCapabilities {
 
     public static Capability<ITimeCounter> TIME_COUNTER_CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
-
-    public static Capability<IFat> FAT_CAPABILITY = CapabilityManager.get(new CapabilityToken<>(){});
 
 }
